7e738a0cd7b1b9710f318e37bfc9799a48cbc18c
Azzouz <menaaziz27@gmail.com>
2021-01-29 03:03:57 +0200

    first

---
+++ src/components/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <section
+      class="text-3xl flex justify-center content-center flex-col mx-auto text-center"
+    >
+      Buy Now
+    </section>
+    <section class="felx text-6xl justify-center content-center">
+      <div class="label text-sm absolute bottom-0">days</div>
+    </section>
+    {{ displayDays }}
+    {{ displayHours }}
+    {{ displayminutes }}
+    {{ displaySeconds }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data: () => ({
+    displayDays: 0,
+    displayHours: 0,
+    displayminutes: 0,
+    displaySeconds: 0,
+  }),
+};
+</script>
+
+<style lang="scss" scoped></style>
