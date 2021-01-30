5b2a295c19db6b01fbca2bd96866450908ca2b27
Azzouz <menaaziz27@gmail.com>
2021-01-30 13:54:06 +0200

    countdown

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,9 +1,10 @@
 <template>
   <div>
-    {{ displayDays }}
-    {{ displayHours }}
-    {{ displayminutes }}
-    {{ displaySeconds }}
+    <h1 style="font-size: 100px">Countdown Vue App</h1>
+    <span> {{ displayDays }} : </span>
+    <span> {{ displayHours }} : </span>
+    <span> {{ displayMinutes }} : </span>
+    <span> {{ displaySeconds }} </span>
   </div>
 </template>
 
@@ -13,10 +14,58 @@ export default {
   data: () => ({
     displayDays: 0,
     displayHours: 0,
-    displayminutes: 0,
+    displayMinutes: 0,
     displaySeconds: 0,
   }),
+  computed: {
+    _seconds: () => {
+      return 1000;
+    },
+    _minutes() {
+      return this._seconds * 60;
+    },
+    _hours() {
+      return this._minutes * 60;
+    },
+    _days() {
+      return this._hours * 24;
+    },
+  },
+  mounted() {
+    this.showRemaining();
+  },
+  methods: {
+    formatNum: (num) => (num < 10 ? "0" + num : num),
+    showRemaining() {
+      const timer = setInterval(() => {
+        const now = new Date();
+        const end = new Date(2021, 1, 2, 2, 10, 10, 10);
+        const distance = end.getTime() - now.getTime();
+
+        // if the timer end
+        if (distance < 0) {
+          clearInterval(timer);
+          return;
+        }
+
+        const days = Math.floor(distance / this._days);
+        const hours = Math.floor((distance % this._days) / this._hours);
+        const minutes = Math.floor((distance % this._hours) / this._minutes);
+        const seconds = Math.floor((distance % this._minutes) / this._seconds);
+
+        // display countdown
+        this.displayDays = this.formatNum(days);
+        this.displayHours = this.formatNum(hours);
+        this.displayMinutes = this.formatNum(minutes);
+        this.displaySeconds = this.formatNum(seconds);
+      }, 1000);
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+span {
+  font-size: 80px;
+}
+</style>
